b1615ea1e4dfffe02cd8622c2c9278b44b01e1a3
Samba Mutthoju <sambmut@cdw.com>
2024-01-04 13:04:46 +0530

    Sample code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,81 +1,8 @@
 import { inject, NgModule } from "@angular/core";
-import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
-import { UserService } from "./core/services/user.service";
-import { map } from "rxjs/operators";
-import { ProfileComponent } from "./features/profile/profile.component";
 
-const routes: Routes = [
-  {
-    path: "",
-    loadComponent: () => import("./features/home/home.component"),
-  },
-  {
-    path: "login",
-    loadComponent: () => import("./core/auth/auth.component"),
-    canActivate: [
-      () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
-    ],
-  },
-  {
-    path: "register",
-    loadComponent: () => import("./core/auth/auth.component"),
-    canActivate: [
-      () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
-    ],
-  },
-  {
-    path: "settings",
-    loadComponent: () => import("./features/settings/settings.component"),
-    canActivate: [() => inject(UserService).isAuthenticated],
-  },
-  {
-    path: "profile",
-    children: [
-      {
-        path: ":username",
-        component: ProfileComponent,
-        children: [
-          {
-            path: "",
-            loadComponent: () =>
-              import("./features/profile/profile-articles.component"),
-          },
-          {
-            path: "favorites",
-            loadComponent: () =>
-              import("./features/profile/profile-favorites.component"),
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "editor",
-    children: [
-      {
-        path: "",
-        loadComponent: () => import("./features/editor/editor.component"),
-        canActivate: [() => inject(UserService).isAuthenticated],
-      },
-      {
-        path: ":slug",
-        loadComponent: () => import("./features/editor/editor.component"),
-        canActivate: [() => inject(UserService).isAuthenticated],
-      },
-    ],
-  },
-  {
-    path: "article/:slug",
-    loadComponent: () => import("./features/article/article.component"),
-  },
-];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
-  ],
-  exports: [RouterModule],
+  imports: [],
+  exports: [],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
